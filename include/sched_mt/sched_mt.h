3a73ab5ea1c8ccb822345be341393977131774f9
fulik0z <fulikozzz@gmail.com>
2026-07-14 12:08:45 +0700

    Описан заголовок симулятора планировщика с многопотоком, для состояния процесса используется структура из core/types.c, для определения алгоритма планирования описана структура sched_type_t

---
+++ include/sched_mt/sched_mt.h
@@ -0,0 +1,55 @@
+#ifndef SCHEDULER_MT_H
+#define SCHEDULER_MT_H
+
+#include <pthread.h>
+#include "../core/types.h"
+
+#define MAX_PROCESSES 16
+
+typedef enum {
+    FIFO,
+    RR,
+    PRIORITY
+} sched_type_t;
+
+// Описание одного потока-процесса
+typedef struct {
+    int id;           
+    int priority;     
+    int burst;      // время работы
+    int arrival;    // задержка перед появлением в очереди
+    int quantum;    // квант времени для RR
+
+    proc_state_t state;
+    int waited;     // время в ожидании
+    int remaining;  // оставшееся время работы для RR
+
+    pthread_cond_t  cond;
+    pthread_mutex_t mutex;
+} sim_proc_t;
+
+typedef struct {
+    sched_type_t type;
+    int quantum; // для RR
+
+    sim_proc_t procs[MAX_PROCESSES];
+    int proc_count;
+
+    // Очередь готовых процессов 
+    int queue[MAX_PROCESSES];
+    int queue_len;
+
+    pthread_mutex_t queue_mutex;
+    pthread_cond_t  queue_cond;
+
+    int active_count; 
+} scheduler_mt_t;
+
+void sched_mt_init(scheduler_mt_t *s, sched_type_t type, int quantum_ms);
+void sched_mt_destroy(scheduler_mt_t *s);
+
+void sched_mt__add_proc(scheduler_mt_t *s, int id, int priority, int burst_ms, int arrival_ms);
+
+void sched_mt_run(scheduler_mt_t *s);
+
+#endif 
